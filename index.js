74d29ec462ea13f2ee8ae0b3ad05d6a6f2e7f3c1
Benito Marino <betoniolomarino@gmail.com>
2025-04-28 15:27:35 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json());
 app.use("/", hltvRoutes);
 app.use("/", llmRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
   console.log(`Servidor geral rodando na porta ${PORT}`);
 });
